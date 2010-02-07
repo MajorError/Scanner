667b1d4d19cc284d5b4af546ccd0443405372aa9
Major_Error <majorerror@happy.(none)>
2010-02-07 10:19:32 +0000

    Repo missing Hooks.cpp!

---
+++ src/Game/Hooks.cpp
@@ -0,0 +1,105 @@
+#include "../Tool.h"
+#include "../VisionProcessor.h"
+#include "../GUICommand.h"
+#include "WorldMap.h"
+#include "AIUnit.h"
+#include <btBulletDynamicsCommon.h>
+#include <TooN/TooN.h>
+
+using namespace CVD;
+using namespace TooN;
+
+MK_VISION_PLUGIN( tick, btClock clock; public: static WorldMap* map; static btDiscreteDynamicsWorld* dynamicsWorld; )
+WorldMap* tick::map = NULL;
+btDiscreteDynamicsWorld* tick::dynamicsWorld = NULL;
+void tick::doProcessing( Image<byte>& sceneBW, Image< Rgb<byte> >& sceneRGB ) {
+    // No map, no game! Disables self
+    if ( map == NULL ) {
+        enabled = false;
+        return;
+    }
+    // Step simulation according to actual time passed - should bypass framerate issues
+    double dt = clock.getTimeMilliseconds();
+    clock.reset();
+    dynamicsWorld->stepSimulation( dt * 0.001f, 100, btScalar(1.)/btScalar(300.) );
+    map->tickAll();
+};
+
+
+MK_TOOL_PLUGIN( game_init, "i", );
+void game_init::click() {
+    commandList::exec( "game.init" );
+};
+
+namespace game {
+    MK_GUI_COMMAND(game, init, GameFactory gf; btDiscreteDynamicsWorld* initBullet();)
+    void game::init( string args ) {
+        GUI.ParseLine( "drawGrid=0" );
+        GUI.ParseLine( "drawEdges=0" );
+        GUI.ParseLine( "drawClosestEdge=0" );
+        GUI.ParseLine( "drawClosestFace=0" );
+        GUI.ParseLine( "textureExtractor.disable" );
+        cerr << "Creating world map" << endl;
+        WorldMap* m = gf.create( environment );
+        cerr << "Creating renderer with " << m << endl;
+        ARDriver::mGame = new GameRenderer( m, environment );
+        cerr << "Starting game" << endl;
+        tick::map = m;
+        tick::dynamicsWorld = initBullet();
+        gf.setupCollisionPlanes( environment, tick::dynamicsWorld );
+        GUI.ParseLine( "tick.enable" );
+    }
+
+    btDiscreteDynamicsWorld* game::initBullet() {
+        cerr << "Bullet init" << endl;
+        int maxProxies = 1024;
+        btVector3 worldAabbMin( -10000, -10000, -10000 );
+        btVector3 worldAabbMax( 10000, 10000, 10000 );
+        btAxisSweep3* broadphase = new btAxisSweep3( worldAabbMin, worldAabbMax, maxProxies );
+
+        // Set up the collision configuration and dispatcher
+        btDefaultCollisionConfiguration* collisionConfiguration = new btDefaultCollisionConfiguration();
+        btCollisionDispatcher* dispatcher = new btCollisionDispatcher( collisionConfiguration );
+
+        // The actual physics solver
+        btSequentialImpulseConstraintSolver* solver = new btSequentialImpulseConstraintSolver;
+
+        // The world.
+        btDiscreteDynamicsWorld* dynamicsWorld = new btDiscreteDynamicsWorld( dispatcher, broadphase, solver, collisionConfiguration );
+        dynamicsWorld->setGravity( btVector3( 0, 0, -10 ) );
+        
+        return dynamicsWorld;
+    };
+}
+
+namespace ai1 {
+    MK_TOOL_PLUGIN( ai_spawn, "a", );
+    void ai_spawn::click() {
+        commandList::exec( "ai.create" );
+    };
+    MK_GUI_COMMAND(ai, create,)
+    void ai::create( string params ) {
+        Vector<3> camPos = environment->getCameraPose().get_translation();
+        AIUnit* a = tick::map->addAI( tick::dynamicsWorld, camPos[0], camPos[1], camPos[2] );
+
+        Matrix<> rot = GV3::get<double>( "pushScale", 100 ) * environment->getCameraPose().get_rotation().get_matrix();
+        a->push( rot[0][2], rot[1][2], rot[2][2] );
+    }
+}
+
+namespace ai2 {
+    MK_GUI_COMMAND(ai, move,)
+    void ai::move( string params ) {
+        stringstream to( params );
+        int id;
+        to >> id;
+        Waypoint* goal = new Waypoint;
+        to >> goal->x >> goal->y >> goal->z;
+        int currID = 0;
+        for( vector<AIUnit*>::iterator curr = tick::map->getUnits().begin(); curr != tick::map->getUnits().end(); curr++, currID++ ) {
+            if ( currID == id ) {
+                (*curr)->navigateTo( goal );
+            }
+        }
+    }
+}
